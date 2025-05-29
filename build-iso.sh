6b61e0ae597faf496a063e0ab0b51485e16799c5
Dani Sarfati <dani@funkervogt.com>
2025-05-29 19:48:08 -0400

    still figuring stuff out

--- build-iso.sh
+++ build-iso.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
 #grub-mkrescue -o bootable.iso iso/
 
+# Make script stop on first error
+set -e
+
+# Create ISO with robust module loading
 grub-mkrescue \
   --output=usbcdrom-bootcd.iso \
-  --install-modules="all_video gfxterm part_msdos fat iso9660 usb uhci ohci ehci usbms chain part_gpt biosdisk search ls help echo sleep font" \
-  --modules="all_video gfxterm part_msdos fat iso9660 usb uhci ohci ehci usbms" \
+  --install-modules="part_msdos fat iso9660 usb uhci ohci ehci usbms chain part_gpt biosdisk normal search ls help" \
+  --modules="part_msdos fat iso9660 usb uhci normal search ls" \
   iso/ \
   -- -volid "USBHLPCD"
   
