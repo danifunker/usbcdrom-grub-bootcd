2829d094fef52410ab1cf23fde2480c12478e29a
Dani S <dani@funkervogt.com>
2025-05-29 18:55:40 -0400

    latest commits

--- build-iso.sh
+++ build-iso.sh
@@ -1,1 +1,7 @@
-grub-mkrescue -o bootable.iso iso/
+#grub-mkrescue -o bootable.iso iso/
+
+grub-mkrescue \
+  --output=usbcdrom-bootcd.iso \
+  --install-modules="usb part_msdos uhci ohci ehci usbms chain part_gpt biosdisk fat iso9660 normal search ls help echo sleep font" \
+  --modules="usb part_msdos uhci ohci ehci usbms chain part_gpt biosdisk fat iso9660 normal search ls help echo sleep font" \
+  iso/
