c7f66892475d5d97a34f14a087aa0345eb9f4959
Dani Sarfati <dani@funkervogt.com>
2025-05-29 19:07:34 -0400

    updating more for ISO build

--- build-iso.sh
+++ build-iso.sh
@@ -4,4 +4,5 @@ grub-mkrescue \
   --output=usbcdrom-bootcd.iso \
   --install-modules="usb part_msdos uhci ohci ehci usbms chain part_gpt biosdisk fat iso9660 normal search ls help echo sleep font" \
   --modules="usb part_msdos uhci ohci ehci usbms chain part_gpt biosdisk fat iso9660 normal search ls help echo sleep font" \
+  --variables="prefix=(cd0)/boot/grub" \
   iso/
