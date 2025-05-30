1e072e05465829d3fb4d5067103d31fdcb4ba631
Dani Sarfati <dani@funkervogt.com>
2025-05-29 20:15:10 -0400

    added most modules for pre 2007 systems

--- build-iso.sh
+++ build-iso.sh
@@ -3,11 +3,11 @@
 # Make script stop on first error
 set -e
 
-# Create ISO with ALL modules included
+# Create ISO with legacy IDE and USB support modules
 grub-mkrescue \
   --output=usbcdrom-bootcd.iso \
-  --install-modules="all" \
-  --modules="ata part_msdos part_gpt fat ntfs ext2 iso9660 udf hfsplus usb uhci ohci ehci usbms pata ahci normal chain search configfile ls cat echo sleep font gfxterm biosdisk boot linux linux16 loadenv minicmd reboot halt memdisk test video" \
+  --install-modules="biosdisk ata pata part_msdos part_gpt fat ntfs ext2 iso9660 udf hfsplus usb uhci ohci ehci usbms normal chain search configfile ls cat echo sleep font gfxterm boot linux linux16 loadenv minicmd reboot halt memdisk test video" \
+  --modules="biosdisk ata pata part_msdos fat iso9660 usb uhci ohci ehci usbms normal search ls linux linux16" \
   iso/ \
   -- -volid "USBHLPCD"
   
