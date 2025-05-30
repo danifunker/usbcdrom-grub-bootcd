111004054d5b0d70c81ed5bf41f7ee7f5559118f
Dani Sarfati <dani@funkervogt.com>
2025-05-29 20:07:12 -0400

    focusing on IDE

--- build-iso.sh
+++ build-iso.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #grub-mkrescue -o bootable.iso iso/
-
 # Make script stop on first error
 set -e
 
-# Create ISO with robust module loading
+# Create ISO with IDE-optimized module loading
 grub-mkrescue \
   --output=usbcdrom-bootcd.iso \
-  --install-modules="part_msdos fat iso9660 usb uhci ohci ehci usbms chain part_gpt biosdisk normal search ls help" \
-  --modules="part_msdos fat iso9660 usb uhci normal search ls" \
+  --install-modules="ata part_msdos fat iso9660 usb uhci ohci ehci usbms chain biosdisk normal search ls help" \
+  --modules="ata part_msdos fat iso9660 usb normal search ls" \
   iso/ \
   -- -volid "USBHLPCD"
   
