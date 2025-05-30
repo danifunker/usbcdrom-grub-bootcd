3f4ad793ea8457da49c091eb7723119e071730d8
Dani Sarfati <dani@funkervogt.com>
2025-05-29 20:45:43 -0400

    getting frustrated

--- build-iso.sh
+++ build-iso.sh
@@ -8,6 +8,7 @@ grub-mkrescue \
   --output=usbcdrom-bootcd.iso \
   --install-modules="biosdisk ata pata part_msdos part_gpt fat ntfs ext2 iso9660 udf hfsplus usb uhci ohci ehci usbms normal chain search configfile ls cat echo sleep font gfxterm boot linux linux16 loadenv minicmd reboot halt memdisk test video" \
   --modules="biosdisk ata pata part_msdos fat iso9660 usb uhci ohci ehci usbms normal search ls linux linux16" \
+  -c iso/boot/grub/grub.cfg \
   iso/ \
   -- -volid "USBHLPCD"
   
